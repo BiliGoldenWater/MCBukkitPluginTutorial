2ba3ed3fbddc1631f8e00c23594a0b1d5530a36f
BiliGoldenWater <2439577268@qq.com>
2020-12-29 14:23:35 +0800

    part2.0

--- src/main/java/indi/goldenwater/mcbukkitplugintutorial/Main.java
+++ src/main/java/indi/goldenwater/mcbukkitplugintutorial/Main.java
@@ -5,4 +5,28 @@ public class Main {
         int a=1;
         System.out.println(a);
     }
+
+//    public static void main(String[] args) {
+//        int a=1;
+//        //a++;
+//    	/*
+//    	a=5;
+//    	a++;
+//    	*/
+//        System.out.println(a);
+//    }
+
+//    public static void main(String[] args) {
+//        int a=1;
+//        int A=2;
+//
+//        System.out.println(a);
+//        System.out.println(A);
+//    }
+
+//    public static void main(String[] args) {
+//        int class=1;
+//
+//        System.out.println(class);
+//    }
 }
